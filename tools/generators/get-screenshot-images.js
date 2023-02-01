8e877c82db1ea0eeabee26b28c8c813059d9e563
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2023-02-01 15:37:11 +0000

    Script to get screenshot images from ES

---
+++ tools/generators/get-screenshot-images.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const { Client } = require('@elastic/elasticsearch')
+const { firefox } = require('playwright');
+
+const imageFolder = 'images/screenshots';
+
+async function run () {
+
+    // Remove old image files
+    fs.rmSync(`${imageFolder}`, { recursive: true, force: true });
+
+    const client = new Client({
+        cloud: { id: process.env.ELASTIC_CLOUD_ID },
+        auth: { apiKey: process.env.ELASTIC_API_KEY }
+    });
+  
+    // Let's search!
+    const result = await client.search({
+      index: 'search-elastic-fu-finder-pages',
+      _source: ['url'],
+      size: 101
+    });
+
+    console.log('Number of results obtained: ' + result.hits.hits.length);
+  
+    getScreenshotsFromHits(result.hits.hits);
+  }
+
+  async function getScreenshotsFromHits(hits) {
+    let browser = await firefox.launch();
+    let page = await browser.newPage();
+    await page.viewportSize({ width: 3840, height: 1080 });
+
+    for (const hit of hits ) {
+      const id = hit._id;
+      const url = hit._source.url;
+
+      console.log(`Obtaining screenshot for ${url}`);
+      
+      try {
+        await page.goto(url);
+        await page.screenshot({ path: `${imageFolder}/${id}.png`, fullPage: true , clip: { x: 0, y: 0, width: 3840, height: 1080 }}); 
+      }
+      catch(e) {
+        console.log(`Timeout received for ${url}`);
+        continue;
+      }
+    }
+
+    await browser.close();
+  }
+  
+  run().catch(console.log)
