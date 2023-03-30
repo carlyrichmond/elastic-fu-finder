5bf03dd0318b63ac71355e5668abaf2e055bf081
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2023-03-30 17:01:10 +0100

    Adding a pacman border to home page image

--- apps/elastic-fu-finder/src/app/home/home.tsx
+++ apps/elastic-fu-finder/src/app/home/home.tsx
@@ -18,13 +18,16 @@ export function Home(props: HomeProps) {
 
   return (
     <div className={styles['container']}>
-        <p>Test your search-fu skills with 
-          <img className={styles['es-logo']} alt="Elasticsearch logo" src="elastic-search-logo-color-64px.svg" /> Elasticsearch!</p>
+        <p>Test your search-fu skills with Elasticsearch!</p>
           <div className={styles['buttons']}>
             <button className={styles['rules-button']} onClick={onRulesSelect}>Rules</button>
             <button className={styles['play-button']} onClick={onPlaySelect}>Play now!</button>
           </div>
-          <img className="splash-image" alt="Man with Magnifying Glass" src="splash.webp" />
+          
+          <div className={styles['pac-man-border']}>
+            <img className={styles['es-logo']} alt="Elasticsearch logo" src="elastic-search-logo-color-64px.svg" />
+            <img className={styles['splash-image']} alt="Man with Magnifying Glass" src="splash.webp" />
+          </div>
     </div>
   );
 }
