71415742a04b221af3288535d8def84bb72d3d4a
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2023-03-30 16:09:15 +0100

    Changing blurb to paragraph

--- apps/elastic-fu-finder/src/app/home/home.tsx
+++ apps/elastic-fu-finder/src/app/home/home.tsx
@@ -18,8 +18,8 @@ export function Home(props: HomeProps) {
 
   return (
     <div className={styles['container']}>
-        <h1>Test your search-fu skills with 
-          <img className={styles['es-logo']} alt="Elasticsearch logo" src="elastic-search-logo-color-64px.svg" /> Elasticsearch!</h1>
+        <p>Test your search-fu skills with 
+          <img className={styles['es-logo']} alt="Elasticsearch logo" src="elastic-search-logo-color-64px.svg" /> Elasticsearch!</p>
           <div className={styles['buttons']}>
             <button className={styles['rules-button']} onClick={onRulesSelect}>Rules</button>
             <button className={styles['play-button']} onClick={onPlaySelect}>Play now!</button>
