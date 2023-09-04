7ed2224caa997b20f157de3d949cdf9c56d0d276
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2023-09-04 14:45:05 +0100

    Updating rules

--- src/components/rules/rules.tsx
+++ src/components/rules/rules.tsx
@@ -5,13 +5,14 @@ export function Rules() {
   return (
     <div className={styles['container']}>
       <p className={styles['rules-paragraph']} data-testid="rules-paragraph">
-        You have 3 minutes to find as many pages as you can! Enter your queries in the search bar and hit enter.
+        You have 3 minutes to find as many pages as you can! 
+        <br/> Enter Elasticsearch queries in the code editor using the fields in the comment and hit the submit button.
       </p>
       
       <img className={styles['game-screenshot']} src='./screen-overview.png' alt='Game Screen Layout'/>
 
       <p className={styles['rules-paragraph']} data-testid="points-paragraph">
-        10 points will be added to your score for each page you find within the 10 results returned by Elasticsearch.
+        10 points will be added to your score for each page you find within the 10 results returned by Elasticsearch. Badges and bonus points will be earned for each different query type used.
       </p>
       <em>Happy searching!</em>
     </div>
