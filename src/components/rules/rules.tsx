179ca4a470a59ab8d0aafee21959585e05dae49f
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2023-11-07 11:36:22 +0000

    Updating rules

--- src/components/rules/rules.tsx
+++ src/components/rules/rules.tsx
@@ -1,6 +1,8 @@
+import { useMediaQuery } from 'react-responsive';
 import styles from './rules.module.scss';
 
 export function Rules() {
+  const isMobile = useMediaQuery({ query: '(max-width: 600px)'});
 
   return (
     <div className={styles['container']}>
@@ -9,7 +11,8 @@ export function Rules() {
         <br/> Enter Elasticsearch queries in the code editor using the fields in the comment and hit the submit button.
       </p>
       
-      <img className={styles['game-screenshot']} src='./screen-overview.png' alt='Game Screen Layout'/>
+      <img className={styles['game-screenshot']} src={ isMobile ? './screen-overview-small.png' : './screen-overview-large.png'} 
+        alt='Game Screen Layout'/>
 
       <p className={styles['rules-paragraph']} data-testid="points-paragraph">
         10 points will be added to your score for each page you find within the 10 results returned by Elasticsearch. Badges and bonus points will be earned for each different query type used.
